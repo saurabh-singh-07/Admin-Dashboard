d28543424918899d7ba01f5f045c5ab66aa31397
Saurabh singh <ajinkyabisht75@gmail.com>
2026-01-15 19:48:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import  Sidebar  from './component/Layout/Sidebar'
+import Header from './component/Layout/Header'
+import Dashboard from './component/Dashboard/Dashboard';
+
+function App(){
+  const [sideBarCollapsed, setSideBarCollapsed] = useState(true);
+  const [currentPage, setCurrentPage] = useState("dashboard");
+  
+  return(
+    <div className='min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500'>
+      <div className='flex h-screen overflow-hidden '>
+        <Sidebar 
+        collapsed= {sideBarCollapsed} 
+        ontoggle ={() => setSideBarCollapsed(!sideBarCollapsed)}
+        currentPage ={currentPage}
+        onPageChange = {setCurrentPage}
+        />
+        <div className='flex-1 flex flex-col overflow-hidden'>
+          <Header 
+          sideBarCollapsed={sideBarCollapsed}
+          onToggleSidebar ={() => setSideBarCollapsed(!sideBarCollapsed)} />
+          <main className='flex-1 overflow-y-auto bg-transparent'>
+            <div className='p-6 space-y-6 '>
+              {currentPage === "dashboard" && <Dashboard/>}
+
+            </div>
+          </main>
+        </div>
+      </div>   
+    </div>
+  )
+}
+export default App
